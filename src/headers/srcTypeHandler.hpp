47667b183e1f867fc9edd5bb300baca8c502c282
Otterwott <cnewman@kent.edu>
2015-01-31 16:30:41 -0500

    Updating srcType

---
+++ src/headers/srcTypeHandler.hpp
@@ -0,0 +1,122 @@
+/**
+ * @file srcSliceHandler.hpp
+ *
+ * @copyright Copyright (C) 2013-2014 SDML (www.srcML.org)
+ *
+ * The srcML Toolkit is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The srcML Toolkit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the srcML Toolkit; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef INCLUDED_SRCSLICEHANDLER_HPP
+#define INCLUDED_SRCSLICEHANDLER_HPP
+
+#include <srcSAXHandler.hpp>
+
+class srcTypeHandler : public srcSAXHandler {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+    /*
+    virtual void startDocument() {}
+    virtual void endDocument() {}
+    */
+
+    /**
+     * startRoot
+     * @param localname the name of the element tag
+     * @param prefix the tag prefix
+     * @param URI the namespace of tag
+     * @param nb_namespaces number of namespaces definitions
+     * @param namespaces the defined namespaces
+     * @param nb_attributes the number of attributes on the tag
+     * @param nb_defaulted the number of defaulted attributes
+     * @param attributes list of attribute name value pairs (localname/prefix/URI/value/end)
+     *
+     * SAX handler function for start of the root element.
+     * Counts the root unit (if an archive, to avoid double count with startUnit).
+     * Overide for desired behaviour.
+     */
+    virtual void startRoot(const char * localname, const char * prefix, const char * URI,
+                           int num_namespaces, const struct srcsax_namespace * namespaces, int num_attributes,
+                           const struct srcsax_attribute * attributes) {
+        
+        //push_element(localname, prefix);
+    }
+    /**
+     * startUnit
+     * @param localname the name of the element tag
+     * @param prefix the tag prefix
+     * @param URI the namespace of tag
+     * @param nb_namespaces number of namespaces definitions
+     * @param namespaces the defined namespaces
+     * @param nb_attributes the number of attributes on the tag
+     * @param nb_defaulted the number of defaulted attributes
+     * @param attributes list of attribute name value pairs (localname/prefix/URI/value/end)
+     *
+     * SAX handler function for start of an unit.
+     * Counts each unit tag (= filecount non-archive, = filecount + 1 if archive).
+     * Overide for desired behaviour.
+     */
+    virtual void startUnit(const char * localname, const char * prefix, const char * URI,
+                           int num_namespaces, const struct srcsax_namespace * namespaces, int num_attributes,
+                           const struct srcsax_attribute * attributes) {
+
+    }
+    /**
+     * startElementNs
+     * @param localname the name of the element tag
+     * @param prefix the tag prefix
+     * @param URI the namespace of tag
+     * @param nb_namespaces number of namespaces definitions
+     * @param namespaces the defined namespaces
+     * @param nb_attributes the number of attributes on the tag
+     * @param nb_defaulted the number of defaulted attributes
+     * @param attributes list of attribute name value pairs (localname/prefix/URI/value/end)
+     *
+     * SAX handler function for start of an element.
+     * Overide for desired behaviour.
+    */
+    virtual void startElement(const char * localname, const char * prefix, const char * URI,
+                                int num_namespaces, const struct srcsax_namespace * namespaces, int num_attributes,
+                                const struct srcsax_attribute * attributes) {
+
+    }
+    /**
+     * charactersUnit
+     * @param ch the characers
+     * @param len number of characters
+     *
+     * SAX handler function for character handling within a unit.
+     * Overide for desired behaviour.
+     */
+    virtual void charactersUnit(const char * ch, int len) {
+
+
+    }
+
+    // end elements may need to be used if you want to collect only on per file basis or some other granularity.
+    virtual void endRoot(const char * localname, const char * prefix, const char * URI) {
+
+    }
+    virtual void endUnit(const char * localname, const char * prefix, const char * URI) {
+
+    }
+
+    virtual void endElement(const char * localname, const char * prefix, const char * URI) {
+        
+    }
+#pragma GCC diagnostic pop
+
+};
+
+#endif
