aacd4be0a6c145f41cfc053c96008c9f0d323eb6
Christian Newman <cnewman@kent.edu>
2015-06-04 12:54:44 -0400

    This test file is used to make an exectuable for srcType instead of a library. It'll eventually be part of a larger set of tests but for now it's good for generating output for srcType without having to link to the library

---
+++ src/tests/srcTypeTest.cpp
@@ -0,0 +1,11 @@
+#include <srcType.hpp>
+#include <iostream>
+std::string SerializeToCppUMap(srcTypeNS::FunctionVarMap mp){
+    return SerializeMapToString(mp);
+}
+int main(int argc, char** argv){
+	srcTypeNS::srcType typeDict;
+	typeDict.ReadArchiveFile(argv[1]);
+	typeDict.SerializeMap(SerializeToCppUMap);
+	std::cerr<<typeDict.size();
+}
